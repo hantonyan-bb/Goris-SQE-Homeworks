1ec227f12e3ebf65c5c51ce1fff1135f61bed541
HaykYeritsyan <43431744+HaykYeritsyan@users.noreply.github.com>
2018-09-24 02:42:29 +0400

    Add files via upload
    Added homework "Squared of Even/Odd number" on 2018-Sep-24

---
+++ homeworks/Hayk_Yeritsyan/SquaredOfNumberEvenOdd.java
@@ -0,0 +1,16 @@
+/*
+
+ Create a program that will display
+ 1. Squared of "n" if "n" is odd
+ 2. 0 if "n" is even
+
+ */
+
+
+public class SquaredOfNumberEvenOdd {
+
+    public static void main(String []args){
+        int n = 21;
+        System.out.println(n*n*(n%2));
+    }
+}
