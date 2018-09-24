5a2eebcab29d9089acdf2940cb419301702932dc
Harut999 <shiraz1995@list.ru>
2018-09-24 17:18:56 +0400

    Adding homeworks

---
+++ homeworks/Harut_Aghajanyan/Tnayin1.java
@@ -0,0 +1,12 @@
+public class Tnayin1 {
+    public static void main(String[] args) {
+
+double inch=0.0254;	    
+int number=7;
+double result = number * inch;
+        System.out.println(result);
+
+
+
+    }
+}
