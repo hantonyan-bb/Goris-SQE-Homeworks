73c60e8d10f0d5cb955e8080bc422ce633a81a6d
AnushMkrtchyan888 <40496663+AnushMkrtchyan888@users.noreply.github.com>
2018-09-24 18:35:45 +0400

    zujgkamkent

---
+++ homeworks/Anush_Mkrtchyan/zujgkam kent.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args) {
+	Scanner scanner=new Scanner(Syste.in);
+	int n=input.nextInt();
+	System.out.println((n%2)*pow(x, 2));
+    }
+}
