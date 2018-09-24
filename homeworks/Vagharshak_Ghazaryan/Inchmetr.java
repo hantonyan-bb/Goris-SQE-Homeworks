00f07090d1da94d74bbe76666911c43348d45675
Vaxarshak <40496725+Vaxarshak@users.noreply.github.com>
2018-09-24 17:53:04 +0400

    Homework

---
+++ homeworks/Vagharshak_Ghazaryan/Inchmetr.java
@@ -0,0 +1,7 @@
+public class Inchmetr {
+    public static void main(String args[]) {
+        int m=3;
+        double i=m*0.0254;
+        System.out.println(i);
+    }
+}
---
+++ homeworks/Vagharshak_Ghazaryan/Qarakusi.java
@@ -0,0 +1,6 @@
+public class Qarakusi  {
+    public static void main(String args[]) {
+        int n=11;
+        System.out.println((n*n)*(n%2));
+    }
+}
