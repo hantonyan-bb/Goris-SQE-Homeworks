758712e0e6b9d4ce442086bdcc7a91655f5d1769
smbathayryan <smbat_h@instigatemobile.com>
2018-09-24 09:53:44 +0400

    Added homeworks

---
+++ homeworks/Smbat_Hayryan/Homework1.java
@@ -0,0 +1,15 @@
+/*
+
+Write a Java program that converts inches to meters.
+
+*/
+
+public class Homework1 {
+    public static void main(String[] args) {
+
+         double value=0.0254;
+         double input=777; // Lyuboy tiv
+         double meterResult = value * input;
+                 System.out.println(meterResult);
+    }
+}
---
+++ homeworks/Smbat_Hayryan/Homework2.java
@@ -0,0 +1,14 @@
+/*
+
+ Given the n number. Write a Java program that prints the square of a number n if n is odd , otherwise print 0. Please do not use conditional statements.
+
+*/
+
+public class Homework2 {
+    public static void main(String[] args) {
+
+         int number=8;
+         System.out.println(number * number * (number%2));
+    }
+}
+
