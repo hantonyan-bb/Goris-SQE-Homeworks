e721a3cd6f31e965ba83bcb4c2928b7afcf3354b
AnushMkrtchyan888 <40496663+AnushMkrtchyan888@users.noreply.github.com>
2018-09-24 18:33:30 +0400

    inches to meters

--- homeworks/Anush_Mkrtchyan/Main.java
+++ homeworks/Anush_Mkrtchyan/Main.java
@@ -1,8 +1,14 @@
+package com.company;
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	Scanner scanner=new Scanner(Syste.in);
-	int n=input.nextInt();
-	System.out.println((n%2)*pow(x, 2));
+        Scanner scanner=new Scanner(System.in);
+        int  m=scanner.nextInt();
+        double inch=0.0254;
+        double metr=m*inch;
+        System.out.println(m+"inch= "+metr+" meters");
     }
 }
