25b83d88d1e71b6eacfe0f0104c830bebe912b5d
Dinunts <dinuntsas@gmail.com>
2018-09-25 19:58:44 +0400

    add Orinak.java

---
+++ homeworks/Arpine_Dinunts/Orinak.java
@@ -0,0 +1,10 @@
+public class Orinak {
+public static void main(String args[]){
+      int x=5;
+      int Fact=1;
+      for (int i=1; i<=x; i++){
+Fact*=i;
+}
+System.out.println(Fact);      }
+}
+
