cbb7fc436821fb81d72d9cd8afd7bce85c618b0d
gbaghdasaryan94 <40435714+gbaghdasaryan94@users.noreply.github.com>
2018-09-26 10:45:07 +0400

    Update Equation.java

--- homeworks/Garnik_Baghdasaryan/Equation.java
+++ homeworks/Garnik_Baghdasaryan/Equation.java
@@ -1,16 +1,16 @@
 public class Equation {
     public static void main (String[]args){
 
-        int a = 1;
-        int b = 4;
-        int c = 4;
+        double a = 1;
+        double b = 4;
+        double c = 4;
         double x1;
         double x2;
 
         if (a<0){
             c=-c;
         }
-        int d = b*b - 4*a*c;
+        double d = b*b - 4*a*c;
         if (d<0){
             System.out.println("Does not make sense");
         }else if (d==0){
