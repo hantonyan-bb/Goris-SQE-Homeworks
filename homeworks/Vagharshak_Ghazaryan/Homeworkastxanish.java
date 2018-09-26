84264abdb5cf70f7b52b5d9ffe20e0cb49c2a8c0
Vaxarshak <40496725+Vaxarshak@users.noreply.github.com>
2018-09-26 17:02:10 +0400

    Homework_astxanish

---
+++ homeworks/Vagharshak_Ghazaryan/Homeworkastxanish.java
@@ -0,0 +1,13 @@
+public class Homeworkastxanish {
+public static void main(String args[]) {
+for (int i = 0; i < 8; i++) {
+         for (int a = 0; a < 8 - i; a++) {
+              System.out.print(" ");
+}
+for (int b = 0; b < i; b++) {
+System.out.print("* ");
+}
+System.out.println();
+}
+}
+}
