52c10f0d3b53125281e2f7fb9e92ce19a72c5653
hantonyan-bb <35562890+hantonyan-bb@users.noreply.github.com>
2018-09-26 20:32:04 +0400

    Checked the homework

--- homeworks/Anush_Mkrtchyan/Revers.java
+++ homeworks/Anush_Mkrtchyan/Revers.java
@@ -10,10 +10,14 @@ public class Revers {
         int first=scanner.nextInt();
         for(i=first; i>=10; ){
             a=i%10;
-            i=i/10;
+            i=i/10; 
+		/*
+		Please be correct. Instead of writting i=i/10 in for loop body, you can write it in increment e.g
+		for(i=first; i>=10; i=i/10 )
+		*/
             end=(end+a)*10;
         }
-       end=end +i;
+       end=end +i; 
         System.out.println(end);
 	}
 }
